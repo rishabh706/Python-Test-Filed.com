5a93e0953f83be9d8f67b9a8dd5eb7e6f874a48f
Rishabh Rahatgaonkar <rishabh706>
2021-03-28 19:41:02 +0530

    second commit

--- app.py
+++ app.py
@@ -2,12 +2,15 @@ from flask import Flask,jsonify,request
 import datetime
 from sqlite import *
 import sqlite3
+import json
 
 app=Flask(__name__)
 
 
 # Creating Table in Database
-create_table()
+table=Database()
+
+table.create_table()
 
 # table_names, table_information = list_tables()
 
@@ -31,7 +34,35 @@ def create():
                 cursor.execute("INSERT INTO SONG(ID,NAME,DURATION,UPLOADED_TIME) VALUES(?,?,?,?)",(unique_id,song_name,song_duration,uploaded_time))
                 cursor.close()
 
-    return jsonify(200)
+            elif audio_file_type=="podcast":
+                unique_id = int(audio_meta_data["ID"])
+                podcast_name = str(audio_meta_data["NAME"])
+                podcast_duration = int(audio_meta_data["DURATION"])
+                uploaded_time = datetime.datetime.now()
+                host=str(audio_meta_data["HOST"])
+                participants=list(audio_meta_data["PARTICIPANTS"])
+                if len(participants)<=10:
+                    participants=json.dumps(participants)
+                    cursor.execute("INSERT INTO PODCAST(ID,NAME,DURATION,UPLOADED_TIME,HOST,PARTICIPANTS) VALUES(?,?,?,?,?,?)",
+                                   (unique_id, podcast_name, podcast_duration, uploaded_time,host,participants))
+                    cursor.close()
+                else:
+                    return jsonify("The request is invalid"),400
+
+            else:
+                unique_id = int(audio_meta_data["ID"])
+                title = str(audio_meta_data["TITLE"])
+                author = str(audio_meta_data["AUTHOR"])
+                narrator=str(audio_meta_data["NARRATOR"])
+                duration=int(audio_meta_data["DURATION"])
+                uploaded_time = datetime.datetime.now()
+                cursor.execute("INSERT INTO AUDIOBOOK(ID,TITLE,AUTHOR,NARRATOR,DURATION,UPLOADED_TIME) VALUES(?,?,?,?,?,?)",
+                               (unique_id,title,author,narrator,duration,uploaded_time))
+                cursor.close()
+
+
+    return jsonify("STATUS OK"),200
+
 
 
 if __name__=="__main__":
--- sqlite.py
+++ sqlite.py
@@ -1,40 +1,51 @@
 import sqlite3
 import pandas as pd
 
-def create_table():
-    # connecting to SQLITE Database
-    with sqlite3.connect('database.db') as con:
+class Database:
 
-        cursor = con.cursor()
+    def __init__(self,table_name=None):
+        self.table_name=table_name
 
-        cursor.execute('CREATE TABLE IF NOT EXISTS SONG (ID INTEGER PRIMARY KEY UNIQUE NOT NULL,NAME VARCHAR(100) NOT NULL,DURATION INTEGER NOT NULL,UPLOADED_TIME DATETIME NOT NULL)')
+    def create_table(self):
+        # connecting to SQLITE Database
+        with sqlite3.connect('database.db') as con:
 
-        cursor.execute(
-            'CREATE TABLE IF NOT EXISTS PODCAST (ID INTEGER PRIMARY KEY UNIQUE NOT NULL,NAME VARCHAR(100) NOT NULL,DURATION INTEGER NOT NULL,UPLOADED_TIME DATETIME NOT NULL,HOST VARCHAR(100) NOT NULL,PARTICIPANTS VARCHAR(100))')
+            cursor = con.cursor()
 
-        cursor.execute(
-            'CREATE TABLE IF NOT EXISTS AUDIOBOOK (ID INTEGER PRIMARY KEY UNIQUE NOT NULL,TITLE VARCHAR(100) NOT NULL,AUTHOR VARCHAR(100) NOT NULL,NARRATOR VARCHAR(100) NOT NULL,DURATION INTEGER NOT NULL,UPLOADED_TIME DATETIME NOT NULL)')
+            cursor.execute('CREATE TABLE IF NOT EXISTS SONG (ID INTEGER PRIMARY KEY UNIQUE NOT NULL,NAME VARCHAR(100) NOT NULL,DURATION INTEGER NOT NULL,UPLOADED_TIME DATETIME NOT NULL)')
 
-        cursor.close()
+            cursor.execute(
+                'CREATE TABLE IF NOT EXISTS PODCAST (ID INTEGER PRIMARY KEY UNIQUE NOT NULL,NAME VARCHAR(100) NOT NULL,DURATION INTEGER NOT NULL,UPLOADED_TIME DATETIME NOT NULL,HOST VARCHAR(100) NOT NULL,PARTICIPANTS VARCHAR(100))')
 
-    return("Table Created Successfully")
+            cursor.execute(
+                'CREATE TABLE IF NOT EXISTS AUDIOBOOK (ID INTEGER PRIMARY KEY UNIQUE NOT NULL,TITLE VARCHAR(100) NOT NULL,AUTHOR VARCHAR(100) NOT NULL,NARRATOR VARCHAR(100) NOT NULL,DURATION INTEGER NOT NULL,UPLOADED_TIME DATETIME NOT NULL)')
 
-def get_table_data(table_name):
+            cursor.close()
 
-    # Connecting to the Database
-    with sqlite3.connect("database.db") as con:
-        # table_names=pd.read_sql_query("SELECT name FROM sqlite_master WHERE type='table'",con)
-        if table_name=="SONG":
-            table_data=pd.read_sql_query("SELECT * FROM SONG",con)
+    def list_tables(self):
+        with sqlite3.connect("database.db") as con:
+            table_names=pd.read_sql_query("SELECT name FROM sqlite_master WHERE type='table'",con)
 
-        elif table_name=="PODCAST":
-            table_data=pd.read_sql_query("SELECT * FROM PODCAST",con)
+        return table_names
 
-        else:
-            table_data=pd.read_sql_query("SELECT * FROM AUDIOBOOK",con)
+    def get_table_data(self,table_name):
 
-    return table_data
+        # Connecting to the Database
+        with sqlite3.connect("database.db") as con:
+            # table_names=pd.read_sql_query("SELECT name FROM sqlite_master WHERE type='table'",con)
+            if self.table_name=="SONG":
+                table_data=pd.read_sql_query("SELECT * FROM SONG",con)
+
+            elif self.table_name=="PODCAST":
+                table_data=pd.read_sql_query("SELECT * FROM PODCAST",con)
+
+            else:
+                table_data=pd.read_sql_query("SELECT * FROM AUDIOBOOK",con)
+
+        return table_data
 
 if __name__=="__main__":
-    table_data=get_table_data("PODCAST")
-    print(table_data)
+    table_data=Database("AUDIOBOOK")
+    table_data.list_tables()
+    data=table_data.get_table_data("AUDIOBOOK")
+    print(data)
